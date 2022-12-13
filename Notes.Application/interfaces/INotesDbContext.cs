03ff8b761688a29bb6d9b547618dd2aba5af3d46
Arcusterrr <nikita.kargapolov000@gmail.com>
2022-12-13 21:12:41 +0500

    s

---
+++ Notes.Application/interfaces/INotesDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Notes.Domain;
+
+namespace Notes.Application.interfaces;
+
+public interface INotesDbContext
+{
+    public DbSet<Note> Notes { get; set; }
+
+    Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+}
---
+++ Notes.Persistence/NotesDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Notes.Application.interfaces;
+using Notes.Domain;
+using Notes.Persistence.EntityTypeConfigurations;
+
+namespace Notes.Persistence;
+
+public class NotesDbContext : DbContext, INotesDbContext
+{
+    public DbSet<Note> Notes { get; set; }
+
+    public NotesDbContext(DbContextOptions<NotesDbContext> dbContextOptions) : base(dbContextOptions){ }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        builder.ApplyConfiguration(new NoteConfiguration());
+        base.OnModelCreating(builder);
+    }
+}
